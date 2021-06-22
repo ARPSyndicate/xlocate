783aa7d97b175d5b3f50c86196e3ac4f7d90aeff
BEAST GLATISANT <26509147+g147@users.noreply.github.com>
2021-06-22 13:58:26 +0530

    Xlocate[1.3]

--- xlocate.py
+++ xlocate.py
@@ -15,8 +15,8 @@ GREEN = '\033[92m'
 YELLOW = '\033[93m'
 CLEAR = '\x1b[0m'
 
-print(BLUE + "Xlocate[1.2] by ARPSyndicate" + CLEAR)
-print(YELLOW + "the ultimate exploit finder" + CLEAR)
+print(BLUE + "Xlocate[1.3] by ARPSyndicate" + CLEAR)
+print(YELLOW + "the ultimate exploits/references finder" + CLEAR)
 
 if len(sys.argv) < 2:
     print(RED + "[!] ./xlocate --help" + CLEAR)
@@ -27,110 +27,86 @@ else:
     parser.add_option('-k', '--keyword', action="store", dest="keys",
                       help="list of keyword to search [jira,wordpress]", default=False)
     parser.add_option('-c', '--cveid', action="store", dest="cves",
-                      help="list of cveid to search [cve-2020-1937,cve-2020-1938]", default=False)
-    parser.add_option('-v', '--verbose', action="store_true",
-                      dest="verbose", help="enable logging", default=False)
+                      help="list of cveid to search [CVE-2020-1937,CVE-2020-1938]", default=False)
     parser.add_option('-t', '--threads', action="store",
-                      dest="threads", help="threads [50]", default=50)
+                      dest="threads", help="threads [100]", default=100)
     parser.add_option('-o', '--output', action="store", dest="output",
-                      help="file for storing the json output", default=False)
-    parser.add_option('-s', '--sources', action="store", dest="sources",
-                      help="sources to use [cvemon,exploitdb]", default="cvemon,exploitdb")
+                      help="path for json output", default=False)
+    parser.add_option('-u', '--unofficial-only', action="store_true", dest="unofficial",
+                      help="locates unofficial exploits/references only", default=False)
+    parser.add_option('-d', '--database', action="store", dest="database",
+                      help="path for database input", default="data.json")
 
 inputs, args = parser.parse_args()
 if not inputs.keys and not inputs.cves:
     parser.error(RED + "[!] input not given" + CLEAR)
 keywords = str(inputs.keys).split(",")
 cveids = str(inputs.cves).split(",")
-sources = str(inputs.sources).split(",")
-verbose = inputs.verbose
+database = str(inputs.database)
+unofficial = inputs.unofficial
 output = str(inputs.output)
 threads = int(inputs.threads)
 result = {}
 
 
 def get_pocs_cvem():
-    response = requests.get(
-        "https://raw.githubusercontent.com/ARPSyndicate/cvemon/master/data.json")
+    if unofficial:
+        response = requests.get(
+            "https://raw.githubusercontent.com/ARPSyndicate/cvemon/master/unofficial-data.json")
+    else:
+        response = requests.get(
+            "https://raw.githubusercontent.com/ARPSyndicate/cvemon/master/data.json")
     return response.json()
 
 
-def get_pocs_exdb():
-    response = requests.get(
-        "https://raw.githubusercontent.com/offensive-security/exploitdb/master/files_exploits.csv")
-    data = []
-    csvio = io.StringIO(response.text, newline="")
-    for row in csv.DictReader(csvio):
-        data.append(row)
-    return data
-
-
-def query_cveb(cve):
-    global cveb
-    response = requests.get(
-        "https://raw.githubusercontent.com/olbat/nvdcve/master/nvdcve/"+cve+".json")
-    summary = response.json(
-    )['cve']['description']['description_data'][0]['value']
-    if verbose:
-        print(GREEN+"[CVEMON] "+cve+CLEAR)
+def query_db(exp):
+    global data, result
     for keyword in keywords:
-        if re.search(keyword, summary, re.IGNORECASE):
-            for poc in cveb[cve]:
-                print(BLUE + "["+keyword+"] " + poc + CLEAR)
-            if keyword in result.keys():
-                result[keyword].extend(cveb[cve])
-            else:
-                result[keyword] = cveb[cve]
-
-
-def query_exdb(keyword):
-    global exdb
-    for exp in exdb:
-        if verbose:
-            print(GREEN+"[EXPLOITDB] "+exp['id']+CLEAR)
-        if re.search(keyword, exp['description'], re.IGNORECASE):
-            poc = "https://www.exploit-db.com/raw/"+exp['id']
-            if keyword in result.keys():
-                if poc not in result[keyword]:
-                    print(BLUE + "["+keyword+"] " + poc + CLEAR)
-                    result[keyword].append(poc)
-            else:
-                print(BLUE + "["+keyword+"] " + poc + CLEAR)
-                result[keyword] = [poc]
+        if re.search(keyword, data[exp], re.IGNORECASE):
+            if "CVE-" in exp:
+                if keyword in result.keys():
+                    result[keyword].extend(cvem[exp.upper()])
+                else:
+                    result[keyword] = cvem[exp.upper()]
+            if "EDB-" in exp:
+                if keyword in result.keys():
+                    result[keyword].append(
+                        "https://www.exploit-db.com/raw/"+exp.split("-")[1])
+                else:
+                    result[keyword] = cvem["https://www.exploit-db.com/raw/" +
+                                           exp.split("-")[1]]
 
 
 if inputs.keys:
-    if "cvemon" in sources:
-        print(GREEN + "[*] gathering exploits from cvemon" + CLEAR)
-        cveb = get_pocs_cvem()
-        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-            try:
-                executor.map(query_cveb, cveb.keys())
-            except(KeyboardInterrupt, SystemExit):
-                executor.shutdown(wait=False)
-                sys.exit()
-
-    if "exploitdb" in sources:
-        print(GREEN + "[*] gathering exploits from exploitdb" + CLEAR)
-        exdb = get_pocs_exdb()
-        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-            try:
-                executor.map(query_exdb, keywords)
-            except(KeyboardInterrupt, SystemExit):
-                executor.shutdown(wait=False)
-                sys.exit()
+    try:
+        with open(database) as db:
+            data = json.load(db)
+    except:
+        print(RED + "[!] invalid database" + CLEAR)
+    cvem = get_pocs_cvem()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        try:
+            executor.map(query_db, list(data.keys()))
+        except(KeyboardInterrupt, SystemExit):
+            executor.shutdown(wait=False)
+            sys.exit()
+    for keyword in keywords:
+        result[keyword] = list(set(result[keyword]))
+        result[keyword].sort()
+        for entry in result[keyword]:
+            print("{3}[{2}] {0} {1}".format(
+                entry, CLEAR, keyword, BLUE))
 
 if inputs.cves:
-    print(GREEN + "[*] gathering exploits from cvemon" + CLEAR)
-    cveb = get_pocs_cvem()
+    cvem = get_pocs_cvem()
     for cve in cveids:
-        if cve.upper() in cveb.keys():
-            for poc in cveb[cve.upper()]:
+        if cve.upper() in cvem.keys():
+            for poc in cvem[cve.upper()]:
                 print(BLUE + "["+cve+"] " + poc + CLEAR)
             if cve in result.keys():
-                result[cve].extend(cveb[cve.upper()])
+                result[cve].extend(cvem[cve.upper()])
             else:
-                result[cve] = cveb[cve.upper()]
+                result[cve] = cvem[cve.upper()]
 
 
 if inputs.output and len(result) > 0:
